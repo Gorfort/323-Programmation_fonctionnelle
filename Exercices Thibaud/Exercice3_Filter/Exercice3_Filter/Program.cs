4d2e5011bf48fa41d21fb360cda83db2d8f6ab23
Thibaud <94205515+Gorfort@users.noreply.github.com>
2024-09-06 12:05:45 +0200

    test

---
+++ Exercices Thibaud/Exercice3_Filter/Exercice3_Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
+
+        double averageLength = words.Average(w => w.Length);
+        // Prends tous les mots qui ne contiennent pas x, qui sont égaux ou plus grand que 4 et qui ont autant de caractères que la moyenne du nombre de caractères de la liste
+        var noX = words.Where(x => !(x.ToLower().Contains('x'))).Where(w => w.Length >= 4).Where(w => w.Length >= averageLength).ToArray();
+        foreach (var word in noX)
+        {
+            Console.WriteLine(word);
+        }
+        Console.Read();
+    }
+}
+
---
+++ Exercices Thibaud/Exercice4Upsilon/Exercice4Upsilon/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<char, double> percentage= new Dictionary<char, double>
+        {
+            {'e', 12.10},
+            {'a', 7.11},
+            {'i', 6.59},
+            {'s', 6.51},
+            {'n', 6.39},
+            {'r', 6.07},
+            {'t', 5.92},
+            {'o', 5.02},
+            {'l', 4.96},
+            {'u', 4.49},
+            {'d', 3.67},
+            {'c', 3.18},
+            {'m', 2.62},
+            {'p', 2.49},
+            {'é', 1.94},
+            {'g', 1.23},
+            {'b', 1.14},
+            {'v', 1.11},
+            {'h', 1.11},
+            {'f', 1.11},
+            {'q', 0.65},
+            {'y', 0.46},
+            {'x', 0.38},
+            {'j', 0.34},
+            {'è', 0.31}
+        };
+
+        Console.Write("Type something: ");
+        string word = Console.ReadLine();
+
+        Func<char, string, int> u = (letter, words) =>
+        {
+            var count = 0;
+            foreach (char c in words)
+            {
+                if (c == letter)
+                    count++;
+            }
+            return count;
+        }; 
+        List<double> result = new List<double>();
+
+        foreach (char w in word)
+        {
+            result.Add(percentage[w] / 100 / u (w, word));  
+        }
+
+        Console.WriteLine(result.Sum());
+        Console.Read();
+    }
+}
